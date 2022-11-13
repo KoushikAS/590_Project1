7b5aaa2277c5c10aa48337ed2eb079189c6a9080
Koushik Annareddy Sreenath <askoushik4@gmail.com>
2022-11-13 12:53:54 -0500

    Fixed Quick sort

--- project1.py
+++ project1.py
@@ -9,88 +9,120 @@ Date: 9-Nov-2022
 """
 
 """
-SelectionSort
+SelectionSort: is a sorting algorithm which works on pushing the lowest element from unsortedlist at the
+                end of every cycle. 
 Best Case: Ω(n^2)
 Average Case: θ(n^2)
 Worst Case: O(n^2)
 Space Complexity: O(1)
 """
-def SelectionSort(listToSort):
-    size = range(0, len(listToSort) - 1)
 
-    for i in size:
+
+def SelectionSort(listToSort):
+    for i in range(0, len(listToSort) - 1):
+        # find out the index which contains the minimum element of the unsorted list
         min_index = i
         for j in range(i + 1, len(listToSort)):
             if listToSort[min_index] > listToSort[j]:
                 min_index = j
-        if min_index != i:
-            listToSort[min_index], listToSort[i] = listToSort[i], listToSort[min_index]
 
-    
+        # swap if the min index is other than i
+        if min_index != i:
+            tmp = listToSort[i]
+            listToSort[i] = listToSort[min_index]
+            listToSort[min_index] = tmp
+
     return listToSort
 
+
 """
-InsertionSort
+InsertionSort: is a sorting algorithm which sorts a subsection of a list every cycle.
 Best Case: Ω(n)
 Average Case: θ(n^2)
 Worst Case: O(n^2)
 Space Complexity: O(1)
 """
-def InsertionSort(listToSort):
-    size = range(1, len(listToSort))
 
-    for i in size:
+
+def InsertionSort(listToSort):
+    for i in range(1, len(listToSort)):
         curr = listToSort[i]
-        while i > 0 and curr < listToSort[i - 1]:
-            listToSort[i], listToSort[i - 1] = listToSort[i - 1], listToSort[i]
-            i = i - 1
+        j = i
+        # push the current element to the appropriate position in the sorted section of the list.
+        while j > 0 and curr < listToSort[j - 1]:
+            tmp = listToSort[j]
+            listToSort[j] = listToSort[j - 1]
+            listToSort[j - 1] = tmp
+            j = j - 1
 
     return listToSort
+
 
 """
 BubbleSort
 """
+
+
 def BubbleSort(listToSort):
     return listToSort
+
 
 """
 MergeSort
 """
+
+
 def MergeSort(listToSort):
     return listToSort
 
+
 """
-QuickSort
+QuickSort: is a recursive sorting algorithm, which works by dividing the list based on a pivot (i.e smaller and 
+            bigger elements than pivot) and then recursively sorts the sublists. 
 Best Case: Ω(n*log(n))
 Average Case: θ(n*log(n))
 Worst Case: O(n^2)
-Space Complexity: O(n*log(n))
+Space Complexity: O(1)
 
 Sort a list with the call QuickSort(listToSort),
 or additionally specify i and j.
 """
-def QuickSort(listToSort, i=0, j=None):
-    listToSort = listToSort[i: j]
 
+
+def QuickSort(listToSort, i=0, j=None):
     # Set default value for j if None.
-    if j == None or j > len(listToSort):
+    if j is None or j > len(listToSort):
         j = len(listToSort)
 
-    if j <= 1:
+    if i >= j:
         return listToSort
-    else:
-        pivot = listToSort.pop()
 
-    higher_list = []
-    lower_list = []
+    # Choosing the last element of the list as the pivot element
+    pivot = listToSort[j - 1]
 
-    for index in range(i, j - 1):
-        if listToSort[index] > pivot:
-            higher_list.append(listToSort[index])
-        else:
-            lower_list.append(listToSort[index])
+    # A pivot index to maintain the point of pivot
+    pivot_index = i
 
-    return QuickSort(lower_list, 0, len(lower_list)) + [pivot] + QuickSort(higher_list, 0, len(higher_list))
+    for k in range(i, j - 1):
+        # Swapping elements which are lesser than pivot at pivot index position.
+        if listToSort[k] < pivot:
+            tmp = listToSort[k]
+            listToSort[k] = listToSort[pivot_index]
+            listToSort[pivot_index] = tmp
+
+            # incrementing the pivot index.
+            pivot_index += 1
+
+    # swapping the pivot to the appropriate place in the list (i.e. pivot index).
+    tmp = listToSort[j - 1]
+    listToSort[j - 1] = listToSort[pivot_index]
+    listToSort[pivot_index] = tmp
+
+    # sorting the lower portion of the list
+    QuickSort(listToSort, i, pivot_index)
+    # sorting the higher portion of the list
+    QuickSort(listToSort, pivot_index + 1, j)
+    return listToSort
 
 """
 Importing the testing code after function defs to ensure same references.
